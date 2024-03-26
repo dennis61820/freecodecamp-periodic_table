47aef3d4dac3430b5f443414b012b4ddd2fe4138
dennis61820 <danngit@hotmail.com>
2024-03-26 14:18:54 -0500

    fixed: element.sh first question

--- element.sh
+++ element.sh
@@ -4,7 +4,7 @@ echo -e "Which element would you want info on?\n"
 read ELEMENT
 if [[ -z $ELEMENT ]]
 then
-    echo -e "\nPlease provide an argument."
+    echo -e "\nPlease provide an element as an argument."
 else
     echo -e "$ELEMENT, good choice."
 fi
